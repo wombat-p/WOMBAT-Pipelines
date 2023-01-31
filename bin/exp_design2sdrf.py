890726e77812bddce4a0f62825d2bfd19c40ce9d
veitveit <veits@bmb.sdu.dk>
2023-01-31 07:38:20 +0100

    config adjustments

--- bin/exp_design2sdrf.py
+++ bin/exp_design2sdrf.py
@@ -2,32 +2,19 @@
 
 import pandas as pd
 
-# Read in the experiment design data from a tab-separated text file
-experiment_design = pd.read_csv("exp_design.txt", sep='\t')
+expd = pd.read_csv("exp_design.txt", sep='\t')
+expd.columns = map(str.lower, expd.columns)  # convert column names to lower-case
+sdrf_out = expd
+# filling in some default values
+sdrf_out.columns = ['comment[file uri]', 'characteristics[experimental samples]']
+sdrf_out['comment[data file]'] = expd['comment[file uri]']
+sdrf_out['comment[instrument]'] = "not available"
+sdrf_out['comment[label]'] = 'AC=MS:1002038;NT=label free sample'
+sdrf_out["comment[cleavage agent details]"] = 'NT=trypsin/P;AC=MS:1001313'
+sample_nums = list(range(1, len(expd.index)+1))
+sdrf_out.insert(0, 'source name', [m + str(n) for m,n in zip(["Sample"]*len(expd.index), sample_nums)])
+sdrf_out['factor value[condition]'] = expd['characteristics[experimental samples]']
 
-# Convert the column names to lower-case
-experiment_design.columns = map(str.lower, experiment_design.columns)
-
-# Create a DataFrame for the SDRF output
-sdrf_output = experiment_design
-
-# Fill in some default values
-sdrf_output.columns = ['comment[file uri]', 'characteristics[experimental samples]']
-sdrf_output['comment[data file]'] = experiment_design['comment[file uri]']
-sdrf_output['comment[instrument]'] = "not available"
-sdrf_output['comment[label]'] = 'AC=MS:1002038;NT=label free sample'
-sdrf_output["comment[cleavage agent details]"] = 'NT=trypsin/P;AC=MS:1001313'
-
-# Generate a list of sample numbers
-sample_nums = list(range(1, len(experiment_design.index)+1))
-
-# Insert a 'source name' column into the DataFrame
-sdrf_output.insert(0, 'source name', ["Sample{}".format(n) for n in sample_nums])
-
-# Copy the 'characteristics[experimental samples]' column to a new 'factor value[condition]' column
-sdrf_output['factor value[condition]'] = experiment_design['characteristics[experimental samples]']
-
-# Save the SDRF output to a tab-separated text file
-with open("sdrf_local.tsv", "w") as f:
-    sdrf_output.to_csv(f, sep="\t", index=False)
+sdrf_out.to_csv("sdrf_local.tsv", sep="\t", index=False)
+    
 
--- bin/sdrf2exp_design.py
+++ bin/sdrf2exp_design.py
@@ -3,40 +3,25 @@
 import pandas as pd
 import os
 
-# Read in the SDRF data from a tab-separated text file
-sdrf_data = pd.read_csv("sdrf_local.tsv", sep='\t')
-
-# Convert all columns to strings
-sdrf_data = sdrf_data.astype(str)
-
-# Convert the column names to lower-case
-sdrf_data.columns = map(str.lower, sdrf_data.columns)
-
-# Create a list of columns that have more than one unique value
-changing_columns = []
-for col in sdrf_data.columns:
+sdrf = pd.read_csv("sdrf_local.tsv", sep='\t')
+sdrf = sdrf.astype(str)
+sdrf.columns = map(str.lower, sdrf.columns)  # convert column names to lower-case
+changing_columns = list()
+for col in sdrf.columns:
     if "factor" in col or "characteristics" in col:
-        if sdrf_data[col].value_counts().size > 1:
+        if sdrf[col].value_counts().size > 1:
             changing_columns.append(col)
 
-# Filter the list of columns to only include those with the "factor" prefix
-changing_columns = [x for x in changing_columns if "factor" in x]
 
-# Create an empty DataFrame for the output
-output_data = pd.DataFrame()
+changing_columns = [x for x in changing_columns if "factor" in x  ]
 
-# Add a 'raw_file' column with the base names of the file URIs
-output_data["raw_file"] = [os.path.basename(item) for item in sdrf_data["comment[file uri]"]]
-
-# Add an 'exp_conditions' column with the concatenated values from the changing columns
+sdrf_out = pd.DataFrame()
+sdrf_out["raw_file"] = [os.path.basename(item) for item in sdrf["comment[file uri]"]]
 if (len(changing_columns) > 0):
-    output_data["exp_conditions"] = sdrf_data[changing_columns].agg('_'.join, axis=1)
+	sdrf_out["exp_conditions"] = sdrf[changing_columns].agg('_'.join, axis=1)
 else: 
-    output_data["exp_conditions"] = "A"
+	sdrf_out["exp_conditions"] = "A"
 
-# Save the output data to a tab-separated text file
-try:
-    with open("exp_design.txt", "w") as f:
-        output_data.to_csv(f, sep="\t", index=False)
-except IOError as e:
-    print("An error occurred while writing the output file:", e)
+sdrf_out.to_csv("exp_design.txt", sep="\t", index=False)
+    
+
