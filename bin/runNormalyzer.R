3b9caea4291ab23685065af268943b907858bdbf
veitveit <veits@bmb.sdu.dk>
2023-02-13 16:15:13 +0100

    better standardization of maxquant output

--- bin/runNormalyzer.R
+++ bin/runNormalyzer.R
@@ -12,13 +12,17 @@ compfile <- strsplit(grep('--comp_file', args, value = TRUE), split = '=')[[1]][
 
 # merging experimental design file from sdrf parser with actual design
 final_exp<-read.csv(exp_file,sep="\t")
-# B<- read.csv("exp_file2.tsv", sep="\t")
-# 
-#final_exp$Experiment <- paste0(make.names(final_exp$source_name), "_Tr_", final_exp$technical_replicate)
-# C<-merge(A,B,by.x="file",by.y="Name")
-# final_exp <- unique(C[,c("Experiment","group")])
-# num_cond <- nrow(final_exp)
-#write.table(final_exp, "Normalyzer_design.tsv",sep="\t", row.names=F,quote=F)
+
+# Create column for (biological) replicate number if not existing already
+if (is.null(final_exp$biorep)) {
+  final_exp$biorep <- 1
+  for (i in unique(final_exp$group)) {
+    ttt <- final_exp[final_exp$group == i, "biorep"]
+    final_exp[final_exp$group == i, "biorep"] <- 1:length(ttt)
+  }
+}
+
+
 
 # comparison set to everything versus first
 if (comps == "") {
@@ -45,7 +49,7 @@ mods <- unlist(sub("\\.site\\.IDs", "", cmods))
 mods <- unlist(gsub("\\.", "", mods))
 names(mods) <- cmods
 for (c in cmods) {
-  peps[,c] <- sapply(peps[,c], function(x) ifelse(is.na(x), "", paste0("_[",mods[c],"]_",unlist(strsplit(as.character(x),";")), collapse="")))
+  peps[,c] <- sapply(peps[,c], function(x) ifelse(is.na(x) | x == "", "", paste0("_[",mods[c],"]_",unlist(strsplit(as.character(x),";")), collapse="")))
 }
 peps$Sequence <- paste0(peps$Sequence, peps[,cmods])
 write.table(peps, "peptide_file.txt", row.names=F, sep="\t", quote=F)
@@ -75,21 +79,34 @@ stats_peptides <- stats_proteins <- NULL
 if (file.exists("NormalyzerPeptides/NormalyzerPeptides_stats.tsv")) {
   stats_peptides <- read.csv("NormalyzerPeptides/NormalyzerPeptides_stats.tsv", sep="\t", row.names=1)
   stats_proteins <- read.csv("NormalyzerProteins/NormalyzerProteins_stats.tsv", sep="\t", row.names=1)
+} else {
+  stats_peptides <- read.csv(paste0("NormalyzerPeptides/", normalyzerMethod ,"-normalized.txt"), sep="\t", row.names=1)
+  stats_proteins <- read.csv(paste0("NormalyzerProteins/", normalyzerMethod ,"-normalized.txt"), sep="\t", row.names=1)
 }
 
 # changing column names
 peptides$missed_cleavages <- peptides$Missed.cleavages
 peptides$charge <- peptides$Charges
 peptides$protein_group <- peptides$Proteins
-if (!is.null(stats_peptides)) {
-  pval_cols <- colnames(stats_peptides)[grep("AdjPVal$", colnames(stats_peptides))]
-  colnames(stats_peptides)[grep("AdjPVal$", colnames(stats_peptides))] <- paste0("differential_abundance_qvalue", sub("_AdjPVal","", pval_cols))
-  pval_cols <- colnames(stats_proteins)[grep("AdjPVal$", colnames(stats_proteins))]
-  colnames(stats_proteins)[grep("AdjPVal$", colnames(stats_proteins))] <- paste0("differential_abundance_qvalue", sub("_AdjPVal","", pval_cols))
-  pval_cols <- colnames(stats_peptides)[grep("PValue$", colnames(stats_peptides))]
-  colnames(stats_peptides)[grep("AdjPVal$", colnames(stats_peptides))] <- paste0("differential_abundance_pvalue", sub("_PValue","", pval_cols))
-  pval_cols <- colnames(stats_proteins)[grep("PValue$", colnames(stats_proteins))]
-  colnames(stats_proteins)[grep("PValue$", colnames(stats_proteins))] <- paste0("differential_abundance_pvalue", sub("_PValue","", pval_cols))
+if (any(grepl("PValue$", colnames(stats_peptides)))) {
+pval_cols <- colnames(stats_peptides)[grep("AdjPVal$", colnames(stats_peptides))]
+colnames(stats_peptides)[grep("AdjPVal$", colnames(stats_peptides))] <- 
+paste0("differential_abundance_qvalue_", sub("_AdjPVal$","", pval_cols))
+pval_cols <- colnames(stats_proteins)[grep("AdjPVal$", colnames(stats_proteins))]
+colnames(stats_proteins)[grep("AdjPVal$", colnames(stats_proteins))] <- 
+paste0("differential_abundance_qvalue_", sub("_AdjPVal$","", pval_cols))
+pval_cols <- colnames(stats_peptides)[grep("PValue$", colnames(stats_peptides))]
+colnames(stats_peptides)[grep("PValue$", colnames(stats_peptides))] <- 
+paste0("differential_abundance_pvalue_", sub("_PValue$","", pval_cols))
+pval_cols <- colnames(stats_proteins)[grep("PValue$", colnames(stats_proteins))]
+colnames(stats_proteins)[grep("PValue$", colnames(stats_proteins))] <- 
+paste0("differential_abundance_pvalue_", sub("_PValue$","", pval_cols))
+pval_cols <- colnames(stats_peptides)[grep("_log2FoldChange$", colnames(stats_peptides))]
+colnames(stats_peptides)[grep("_log2FoldChange$", colnames(stats_peptides))] <- 
+paste0("log_fold_change_", sub("_log2FoldChange$","", pval_cols))
+pval_cols <- colnames(stats_proteins)[grep("_log2FoldChange$", colnames(stats_proteins))]
+colnames(stats_proteins)[grep("_log2FoldChange$", colnames(stats_proteins))] <- 
+paste0("log_fold_change_", sub("_log2FoldChange$","", pval_cols))
 }
 
 colnames(peptides) <- unlist(sub("^Experiment\\.", "number_of_psms_", colnames(peptides)))
@@ -104,14 +121,14 @@ colnames(peptides) <- unlist(sub("^Experiment\\.", "number_of_psms_", colnames(p
 colnames(proteins) <- unlist(sub("^Razor\\.\\.\\.unique\\.peptides\\.", "number_of_peptides_", colnames(proteins)))
 proteins$protein_group <- rownames(proteins)
 for (s in 1:nrow(final_exp))  {
-  substitute_from <- paste0(make.names(final_exp$source_name[s]),"_Tr_",final_exp$technical_replicate[s])
-  substitute_with <- paste0(make.names(final_exp$group[s]),"_",final_exp$technical_replicate[s])
-  # avoid setting X in front 
+substitute_from <- paste0(make.names(final_exp$source_name[s]),"_Tr_",final_exp$technical_replicate[s])
+substitute_with <- paste0(make.names(final_exp$group[s]),"_",final_exp$biorep[s])
+# avoid setting X in front 
 #  if (grepl("^X", substitute_with)) substitute_with <- sub("^X","",substitute_with)
-  colnames(proteins) <- sub(substitute_from, substitute_with, colnames(proteins))
-  colnames(peptides) <- sub(substitute_from, substitute_with, colnames(peptides))
-  colnames(norm_proteins) <- sub(paste0("^",substitute_from), paste0("abundance_", substitute_with), colnames(norm_proteins))
-  colnames(norm_peptides) <- sub(paste0("^",substitute_from), paste0("abundance_", substitute_with), colnames(norm_peptides))
+colnames(proteins) <- sub(substitute_from, substitute_with, colnames(proteins))
+colnames(peptides) <- sub(substitute_from, substitute_with, colnames(peptides))
+colnames(norm_proteins) <- sub(paste0("^",substitute_from), paste0("abundance_", substitute_with), colnames(norm_proteins))
+colnames(norm_peptides) <- sub(paste0("^",substitute_from), paste0("abundance_", substitute_with), colnames(norm_peptides))
 }
 colnames(proteins) <- make.unique(colnames(proteins))
 colnames(peptides) <- make.unique(colnames(peptides))
@@ -121,20 +138,20 @@ colnames(norm_peptides) <- make.unique(colnames(norm_peptides))
 
 # getting relevant columns
 norm_peptides[,grep("^abundance_", colnames(norm_peptides), value=T)] <- 2^(norm_peptides[,grep("^abundance_", colnames(norm_peptides), value=T)])
-if (is.null(stats_peptides)) {
-  proteins <- cbind(proteins[rownames(norm_proteins), c("protein_group", grep("^number_of_peptides_", colnames(proteins), value=T))], 
-                  norm_proteins[,grep("^abundance_", colnames(norm_proteins), value=T)])
-  peptides <- cbind(modified_peptide=rownames(norm_peptides), 
-                  peptides[rownames(norm_peptides), c("protein_group", grep("^number_of_psms_", colnames(peptides), value=T))],
-                  norm_peptides[,grep("^abundance_", colnames(norm_peptides), value=T)])
+if (!any(grepl("^differential_abundance", colnames(stats_peptides)))) {
+proteins <- cbind(proteins[rownames(norm_proteins), c("protein_group", grep("^number_of_peptides_", colnames(proteins), value=T))], 
+              norm_proteins[,grep("^abundance_", colnames(norm_proteins), value=T)])
+peptides <- cbind(modified_peptide=rownames(norm_peptides), 
+              peptides[rownames(norm_peptides), c("protein_group", grep("^number_of_psms_", colnames(peptides), value=T))],
+              norm_peptides[,grep("^abundance_", colnames(norm_peptides), value=T)])
 } else {
-  proteins <- cbind(proteins[rownames(norm_proteins), c("protein_group", grep("^number_of_peptides_", colnames(proteins), value=T))], 
-                  stats_proteins[rownames(norm_proteins), grep("^differential_abundance", colnames(stats_proteins), value=T)],
-                  norm_proteins[,grep("^abundance_", colnames(norm_proteins), value=T)])
-  peptides <- cbind(modified_peptide=rownames(norm_peptides), 
-                  peptides[rownames(norm_peptides), c("protein_group", grep("^number_of_psms_", colnames(peptides), value=T))],
-                  norm_peptides[,grep("^abundance_", colnames(norm_peptides), value=T)],
-                  stats_peptides[rownames(norm_peptides), grep("^differential_abundance", colnames(stats_peptides), value=T)])
+proteins <- cbind(proteins[rownames(norm_proteins), c("protein_group", grep("^number_of_peptides_", colnames(proteins), value=T))], 
+              stats_proteins[rownames(norm_proteins), grep("^differential_abundance", colnames(stats_proteins), value=T)],
+              norm_proteins[,grep("^abundance_", colnames(norm_proteins), value=T)])
+peptides <- cbind(modified_peptide=rownames(norm_peptides), 
+              peptides[rownames(norm_peptides), c("protein_group", grep("^number_of_psms_", colnames(peptides), value=T))],
+              norm_peptides[,grep("^abundance_", colnames(norm_peptides), value=T)],
+              stats_peptides[rownames(norm_peptides), grep("^differential_abundance", colnames(stats_peptides), value=T)])
 }
 
 
