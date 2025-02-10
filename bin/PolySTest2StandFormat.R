325044a4f5490c02faf91e5ffadc06016e0f04ff
veitveit <veits@bmb.sdu.dk>
2025-02-10 18:01:04 +0100

    adding ions to polystest

--- bin/PolySTest2StandFormat.R
+++ bin/PolySTest2StandFormat.R
@@ -131,7 +131,7 @@ stand_peps <- stand_peps[order(stand_peps$protein_group), ]
 write.csv(stand_peps, "stand_pep_quant_merged.csv", row.names = F)
 
 # Still needs more adjustments of colnames, ...
-write.csv(ions, "stand_ions_quant_merged.csv", row.names = F)
+write.csv(stand_ions, "stand_ions_quant_merged.csv", row.names = F)
 
 
 # Converting column names
