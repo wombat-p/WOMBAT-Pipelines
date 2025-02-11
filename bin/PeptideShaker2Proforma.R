bd6a7caa48ea4b4bd038fec5378f5aa2ce5f1f3a
veitveit <veits@bmb.sdu.dk>
2025-02-11 15:38:14 +0100

    correcting sequence in peptideshaker output

--- bin/PeptideShaker2Proforma.R
+++ bin/PeptideShaker2Proforma.R
@@ -12,6 +12,7 @@ print(names(peptides))
 modify_sequence <- function(fmods, vmods, sequence) {
   modified_peptides <- lapply(paste0(fmods, ";", vmods), function(x) strsplit(x, ";")[[1]])
   modified_peptides <- lapply(modified_peptides, function(y) {
+    print(y)
     mm <- lapply(y, function(x) {
       if (any(!is.na(x))) {
         # Extract string in parentheses
