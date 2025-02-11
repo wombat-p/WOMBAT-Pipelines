f0292499b8ea16c60201524b8349a99c4a43ef38
veitveit <veits@bmb.sdu.dk>
2025-02-11 16:24:54 +0100

    correcting sequence in peptideshaker output

--- bin/PeptideShaker2Proforma.R
+++ bin/PeptideShaker2Proforma.R
@@ -54,12 +54,10 @@ modify_sequence <- function(fmods, vmods, sequence) {
   return(modified_sequence)
 }
 
-peptides$modified_sequence <- modify_sequence(peptides$Variable.Modifications, peptides$Fixed.Modifications, peptides$Sequence)
-print(cbind(peptides$modified_sequence, peptides$Variable.Modifications, peptides$Fixed.Modifications, peptides$Sequence))
-exit()
-ions$modified_sequence <- modify_sequence(ions$Variable.Modifications, ions$Fixed.Modifications, ions$Sequence)
+peptides$Modified.Sequence <- modify_sequence(peptides$Variable.Modifications, peptides$Fixed.Modifications, peptides$Sequence)
+ions$Modified.Sequence <- modify_sequence(ions$Variable.Modifications, ions$Fixed.Modifications, ions$Sequence)
 
-# Reduce protein accessions from long format (e.g. "sp|P12345|A1BG_HUMAN;sp|P12346|A1BG_HUMAN") to a string of only the accession numbers
+# Not needed: Reduce protein accessions from long format (e.g. "sp|P12345|A1BG_HUMAN;sp|P12346|A1BG_HUMAN") to a string of only the accession numbers
 reduce_prot_accs <- function(accessions) {
   tout <- sapply(accessions, function(y) {
     tgroup <- unlist(strsplit(y, "; "))
@@ -78,45 +76,5 @@ reduce_prot_accs <- function(accessions) {
   return(tout)
 }
 
-proteins$samesets_accessions <- reduce_prot_accs(proteins$samesets_accessions)
-peptides$samesets_accessions <- reduce_prot_accs(peptides$samesets_accessions)
-ions$samesets_accessions <- reduce_prot_accs(ions$samesets_accessions)
-
-stand_peps <- data.frame(
-  "modified_peptide" = peptides$modified_sequence, protein_group = peptides$samesets_accessions,
-  peptides[, grep("^number_of_psms", colnames(peptides)), drop = F],
-  2^peptides[, grep("^abundance", colnames(peptides)), drop = F],
-  peptides[, grep("^log_ratios", colnames(peptides)), drop = F],
-  peptides[, grep("^differential_abundance_qvalue", colnames(peptides)), drop = F]
-)
-
-stand_ions <- data.frame(
-  modified_peptide = ions$modified_sequence,
-  protein_group = ions$samesets_accessions,
-  charge = ions$master_quant_peptide_ion_charge,
-  ions[, grep("^number_of_psms", colnames(ions)), drop = F],
-  ions[, grep("^abundance", colnames(ions)), drop = F]
-)
-
-# deleting charge states with lower intensities to maintain max. 1 (modified) peptide sequence
-stand_peps <- stand_peps[order(rowMeans(peptides[, grep("^abundance", colnames(peptides))], na.rm = T), decreasing = T), ]
-stand_peps <- stand_peps[!duplicated(stand_peps$modified_peptide), ]
-stand_peps <- stand_peps[order(stand_peps$protein_group), ]
-write.csv(stand_peps, "stand_pep_quant_merged.csv", row.names = F)
-
-# Still needs more adjustments of colnames, ...
-write.csv(stand_ions, "stand_ions_quant_merged.csv", row.names = F)
-
-
-# Converting column names
-colnames(proteins) <- sub("^log\\.ratios\\.", "log_ratios_", colnames(proteins))
-for (s in unique(exp_design$exp_condition)) colnames(proteins) <- sub(paste0("^", s, "\\."), paste0("abundance_", s, "_"), colnames(proteins))
-colnames(proteins) <- sub("^FDR\\.PolySTest\\.[X]?", "differential_abundance_qvalue_", colnames(proteins))
-stand_prots <- data.frame(
-  protein_group = proteins$samesets_accessions,
-  proteins[, grep("^number_of_peptides", colnames(proteins)), drop = F],
-  proteins[, grep("^abundance_", colnames(proteins)), drop = F],
-  proteins[, grep("^log_ratios", colnames(proteins)), drop = F],
-  proteins[, grep("^differential_abundance_qvalue_", colnames(proteins)), drop = F]
-)
-write.csv(stand_prots, "stand_prot_quant_merged.csv", row.names = F)
+write.csv(peptides, "peptides_proforma.txt", row.names = F, sep = "\t")
+write.csv(ions, "psms_proforma.txt", row.names = F, sep = "\t")
