ab71345119d5ce70a52bca2c47e95b73e2ffa7ce
veitveit <veits@bmb.sdu.dk>
2025-02-12 12:28:47 +0100

    correcting sequence in peptideshaker output

--- bin/PeptideShaker2Proforma.R
+++ bin/PeptideShaker2Proforma.R
@@ -59,12 +59,12 @@ modify_sequence <- function(fmods, vmods, sequence) {
       )
     }
   }
-  print(modified_sequence)
 
   return(modified_sequence)
 }
 
 peptides$"Modified Sequence" <- modify_sequence(peptides$"Variable Modifications", peptides$"Fixed Modifications", peptides$Sequence)
+print(peptides$"Modified Sequence")
 ions$"Modified Sequence" <- modify_sequence(ions$"Variable Modifications", ions$"Fixed Modifications", ions$Sequence)
 
 # Not needed: Reduce protein accessions from long format (e.g. "sp|P12345|A1BG_HUMAN;sp|P12346|A1BG_HUMAN") to a string of only the accession numbers
