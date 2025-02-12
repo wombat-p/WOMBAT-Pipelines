cf52ade72e4b873c68a0a0379ac7f3ff6dbf511b
veitveit <veits@bmb.sdu.dk>
2025-02-12 12:42:01 +0100

    correcting sequence in peptideshaker output

--- bin/PeptideShaker2Proforma.R
+++ bin/PeptideShaker2Proforma.R
@@ -52,6 +52,7 @@ modify_sequence <- function(fmods, vmods, sequence) {
   for (i in 1:length(modified_peptides)) {
     x <- unlist(modified_peptides[[i]])
     print(x)
+    x <- x[order(as.numeric(x[, 2]))]
     if (!is.null(x)) {
       modified_sequence[i] <- stri_sub_replace_all(modified_sequence[i],
         replacement = paste0("[", x[, 1], "]"),
